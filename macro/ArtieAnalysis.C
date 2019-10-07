6b6a5381ebd6ffeda37df30ab2b9cdfaff7aa4b1
Jingbo Wang <jiowang@ucdavis.edu>
2019-10-07 16:50:59 -0500

    Fixed bugs in macro/ArtieAnalysis.C

--- macro/ArtieAnalysis.C
+++ macro/ArtieAnalysis.C
@@ -1,3 +1,5 @@
+
+
 #include "ArtieStyle.C"
 //#include "ArtieTree.h"
 #include "ArtieTree.C"
@@ -6,17 +8,24 @@
 #include <TCanvas.h>   
 
 //////////////////////////////////// Parameters ////////////////////////////////////
-bool UseTrueEnergy = false;
-bool UseLogScale = true;
-double T0Res = 125.0; //ns
-double TDetRes = 1.0; //ns
-TString TimeSmearOption = "MC"; // "MC" or "Gaus"
-TRandom3 randgen;
+bool _UseTrueEnergy = false;
+bool _UseLogScale = true;
+double _T0Res = 125.0; //ns
+double _TDetRes = 1.0; //ns
+double _FlightPath = 71.9; //m
+double _Energy_min = 40.0; // keV
+double _Energy_max = 70.0; // keV
+TString _TimeSmearOption = "MC"; // "MC" or "Gaus"
+TRandom3 _randgen;
+// will create a configuration file to store the parameters
 
 //////////////////////////////////// Histograms ////////////////////////////////////
 // beam energy 
 TH1D *hBeam_Energy = 0; 
-// True energy from MC
+// time of flight
+TH1D *hTof_argon = 0;
+TH1D *hTof_vacuum = 0;
+// neutron energy
 TH1D *hEnergy_argon = 0;
 TH1D *hEnergy_vacuum = 0; 
 TH1D *hTransmission = 0;
@@ -65,36 +74,39 @@ void ArtieAnalysis(TString argonfile, TString vacuumfile){
      targ.fChain->GetEntry(jentry);
      tvac.fChain->GetEntry(jentry);
      hBeam_Energy->Fill(targ.gen_energy*1.e6); // convert to eV
-     if(UseTrueEnergy) { 
+     if(_UseTrueEnergy) { 
        hEnergy_argon->Fill(targ.arrival_e*1000); // convert to keV
        hEnergy_vacuum->Fill(tvac.arrival_e*1000); // convert to keV  	
      }
      else {
      	 // get true time-of-flight
-     	 double tof_arg = targ.arrival_time/1000;
-     	 double tof_vac = tvac.arrival_time/1000;
-     	 if(TimeSmearOption == "MC") {
+     	 double tof_arg = targ.arrival_time/1000; // ns -> us
+     	 double tof_vac = tvac.arrival_time/1000; // ns -> us
+     	 if(_TimeSmearOption == "MC") {
      	 	 // use true energy and true tof to determine the relative moderator smearing
      	   double tof_arg_smear = GetTimeSmear(targ.arrival_e, hModeratorFunction);
      	   double tof_vac_smear = GetTimeSmear(tvac.arrival_e, hModeratorFunction);
      	 	 tof_arg = tof_arg*(1+tof_arg_smear);
          tof_vac = tof_vac*(1+tof_vac_smear);
      	 }
-       else if(TimeSmearOption == "Gaus") {
+       else if(_TimeSmearOption == "Gaus") {
        	 // use Gaussian moderator response 
-       	 double tof_arg_smear = randgen.Gaus(0, T0Res/1000/2.35); // FWHM -> sigma
-     	   double tof_vac_smear = randgen.Gaus(0, T0Res/1000/2.35); // FWHM -> sigma
+       	 double tof_arg_smear = _randgen.Gaus(0, _T0Res/1000/2.35); // FWHM -> sigma
+     	   double tof_vac_smear = _randgen.Gaus(0, _T0Res/1000/2.35); // FWHM -> sigma
      	   tof_arg += tof_arg_smear;
          tof_vac += tof_vac_smear;
        }
      	 
      	 // add detector time resolution
-     	 tof_arg += randgen.Gaus(0, TDetRes/1000);
-     	 tof_vac += randgen.Gaus(0, TDetRes/1000);
+     	 tof_arg += _randgen.Gaus(0, _TDetRes/1000);
+     	 tof_vac += _randgen.Gaus(0, _TDetRes/1000);
+     	 
+     	 hTof_argon->Fill(tof_arg*1000); // ns
+     	 hTof_vacuum->Fill(tof_vac*1000); // ns
      	 
      	 // get energy from TOF
-     	 double energy_arg = GetEnergyFromTOF(tof_arg); // ns -> keV
-     	 double energy_vac = GetEnergyFromTOF(tof_vac); // ns -> keV
+     	 double energy_arg = GetEnergyFromTOF(tof_arg); // us -> keV
+     	 double energy_vac = GetEnergyFromTOF(tof_vac); // us -> keV
      	 
        hEnergy_argon->Fill(energy_arg); 
        hEnergy_vacuum->Fill(energy_vac);
@@ -139,8 +151,9 @@ void ArtieAnalysis(TString argonfile, TString vacuumfile){
 //////////////////////////////////// Get energy from Time-of-Flight  ////////////////////////////////////
 double GetEnergyFromTOF(double tof){
   double MN = 939.*1000.0;  // keV 
-  double C  = 3.e2;         // m / mus
-  double L = 71.03;        //  m
+  double C  = 3.e2;         // m / us
+  //double L = 71.03;        //  m
+  double L = _FlightPath;
   double beta = (L / tof) / C;
   double gamma = sqrt(1.0 / (1 - beta*beta));
   return (gamma - 1.0)*MN;    
@@ -149,8 +162,9 @@ double GetEnergyFromTOF(double tof){
 // Get Time-of-Flight from Energy
 double GetTofFromEnergy(double e){
   double MN = 939.*1000.0;  // keV 
-  double C  = 3.e2;         // m / mus
-  double L = 71.03;        //  m
+  double C  = 3.e2;         // m / us
+  //double L = 71.03;        //  m
+  double L = _FlightPath;
   double gamma = sqrt(1-1/(1+e/MN)/(1+e/MN));
   return L/C/gamma;
 }
@@ -173,16 +187,23 @@ void Init() {
   hBeam_Energy = new TH1D("hBeam_Energy", "", nlogbins, xbins);
 	
 // Make fix-bin histograms
-	double emin  = 40.;
-  double emax  = 70.;
-  double nbins = 30;
+	int emin  = _Energy_min;
+  int emax  = _Energy_max;
+  int nbins = emax-emin;
   
+  int tofmin = TMath::Floor(GetTofFromEnergy(emax)*1000); // keV -> ns
+  int tofmax = TMath::Ceil(GetTofFromEnergy(emin)*1000); // keV -> ns
+  int ntofbins = (tofmax - tofmin)/100; // 10 ns per bin
+  cout<<"tofmin, tofmax = "<<tofmin<<", "<<tofmax<<endl;
+  
+  hTof_argon = new TH1D("hTof_argon", "Argon-fill: Time-of-Flight", ntofbins, tofmin, tofmax);
+  hTof_vacuum = new TH1D("hTof_vacuum", "Vacuum-fill: Time-of-Flight", ntofbins, tofmin, tofmax);
   hEnergy_argon = new TH1D("hEnergy_argon", "Argon-fill: energy spectrum", nbins, emin, emax);
   hEnergy_vacuum = new TH1D("hEnergy_vacuum", "Vacuum-fill: energy spectrum", nbins, emin, emax);
   hTransmission = new TH1D("hTransmission", "Transmission Coefficient", nbins, emin, emax);
   hCrossSection = new TH1D("hCrossSection", "Total cross-section", nbins, emin, emax);
   
-  ArtieSimulation();
+  ArtieSimulation(); // need a fix!
 }
 
 //////////////////////////////////// Moderator resonse ////////////////////////////////////
@@ -215,6 +236,31 @@ void Plot() {
   hBeam_Energy->GetYaxis()->SetTitle("Count [/bin]");
   hBeam_Energy->Draw("HIST");
   hBeam_Energy->Write();
+  
+  ir++;
+  c[ir] = new TCanvas(Form("c%d", ir), Form("c%d", ir), 20*ir, 20*ir, 800, 600);
+  c[ir]->cd();	          
+  hTof_vacuum->GetXaxis()->SetTitle("Time-of-Flight [ns]");
+  hTof_vacuum->GetYaxis()->SetTitle("Count [/10ns]");
+  hTof_vacuum->GetYaxis()->SetRangeUser(0,hEnergy_vacuum->GetMaximum()*1.6);
+  hTof_vacuum->SetLineColor(4);
+  hTof_vacuum->SetMarkerStyle(20);
+  hTof_vacuum->SetMarkerColor(4);    
+  hTof_vacuum->SetMinimum(0);
+  hTof_vacuum->Draw("PE");
+  hTof_argon->GetXaxis()->SetTitle("Time-of-Flight [ns]");
+  hTof_argon->GetYaxis()->SetTitle("Count [/10ns]");
+  hTof_argon->SetLineColor(2);
+  hTof_argon->SetMarkerStyle(20);
+  hTof_argon->SetMarkerColor(2);
+  hTof_argon->Draw("PEsame");
+  hTof_vacuum->Write();
+  hTof_argon->Write();
+  
+  TLegend *legend1 = new TLegend(0.55,0.7,0.85,0.85);
+  legend1->AddEntry(hEnergy_vacuum,"Vacuum-fill","l");
+  legend1->AddEntry(hEnergy_argon,"Argon-fill","l");
+  legend1->Draw("same");
 	
   ir++;
   c[ir] = new TCanvas(Form("c%d", ir), Form("c%d", ir), 20*ir, 20*ir, 800, 600);
@@ -233,6 +279,8 @@ void Plot() {
   hEnergy_argon->SetMarkerStyle(20);
   hEnergy_argon->SetMarkerColor(2);
   hEnergy_argon->Draw("PEsame");
+  hEnergy_vacuum->Write();
+  hEnergy_argon->Write();
   
   TLegend *legend2 = new TLegend(0.55,0.7,0.85,0.85);
   legend2->AddEntry(hEnergy_vacuum,"Vacuum-fill","l");
@@ -270,15 +318,15 @@ void Plot() {
   hModeratorFunction->GetYaxis()->SetTitle("#DeltaT/T_{true}");
   hModeratorFunction->Draw("colz");
   
-  ir++;
-  c[ir] = new TCanvas(Form("c%d", ir), Form("c%d", ir), 20*ir, 20*ir, 800, 600);
-  c[ir]->cd();
-  TAxis *xaxis = hModeratorFunction->GetXaxis();
-  int binx = xaxis->FindBin(57./1000);
-  TH1D *hist_py = hModeratorFunction->ProjectionY("", binx, binx+1);
-  hist_py->SetName("hsmear");
-  hist_py->Draw("HIST");
-  hist_py->Write();
+//  ir++;
+//  c[ir] = new TCanvas(Form("c%d", ir), Form("c%d", ir), 20*ir, 20*ir, 800, 600);
+//  c[ir]->cd();
+//  TAxis *xaxis = hModeratorFunction->GetXaxis();
+//  int binx = xaxis->FindBin(57./1000);
+//  TH1D *hist_py = hModeratorFunction->ProjectionY("", binx, binx+1);
+//  hist_py->SetName("hsmear");
+//  hist_py->Draw("HIST");
+//  hist_py->Write();
   
   f->Close();
 }
